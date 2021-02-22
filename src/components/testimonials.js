1f139bed8c41a4e45618db8c113027fc7a6c23c1
Kim Kiamco <kim.kiamco@menlosecurity.com>
2021-02-21 19:56:31 -0800

    added testimonials

---
+++ src/components/testimonials.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import { Box, Button, Typography, Avatar, IconButton } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { mainBlue, secondaryFont } from './themes/color';
+import { primaryBtn } from './themes/components';
+
+const useStyles = makeStyles({
+    root:{
+
+    }
+})
+
+const Testimonials = (props) => {
+    return (
+        <Box className={classes.root}>
+            <Box className={classes.left}>
+
+            </Box>
+            <Box className={classes.right}>
+ 
+            </Box>
+            <Box className={classes.desc}>
+                
+            </Box>
+        </Box>
+    )
+}
